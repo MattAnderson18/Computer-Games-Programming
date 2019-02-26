a474d1e39f428adad39b30a4d064818a48cd9b1f
MattAnderson18 <anderson.matthew84@gmail.com>
2019-02-26 12:57:21 +0000

    Week 4 - Drawing Vector Shapes

---
+++ Week 4/ShapeRepresentation/ShapeRepresentation/BresCircle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ShapeRepresentation
+{
+    public partial class BresCircle : Form
+    {
+        int centreX, centreY, radius;
+        Point plotPt;
+
+        //Constructor
+        public BresCircle()
+        {
+            // Size the frame to full screen 
+            this.WindowState = FormWindowState.Maximized;
+
+            InitializeComponent();
+
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.BackColor = Color.Black;
+
+            centreX = 200;   // fixed values for illustration
+            centreY = 200;
+            radius = 150;
+            plotPt = new Point(0, 0);
+        }
+
+        // Method fills in one pixel only
+        void putPixel(Graphics g, Point pixel, Brush aBrush)
+        {
+            // FillRectangle call fills at location x y and is 1 pixel high by 1 pixel wide
+            g.FillRectangle(aBrush, pixel.X, pixel.Y, 1, 1);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; // Get on screen graphics tool
+            drawCircle(g);
+        }
+
+        void drawCircle(Graphics g)
+        {
+            int x = 0;
+            int y = radius;
+            int d = 3 - 2 * radius;  // initial value
+
+            while (x <= y)
+            {
+                // put pixel in each octant
+                plotPt.X = x + centreX;
+                plotPt.Y = y + centreY;
+                putPixel(g, plotPt, (Brush) Brushes.Red);
+
+                plotPt.X = y + centreX;
+                plotPt.Y = x + centreY;
+                putPixel(g, plotPt, (Brush) Brushes.Orange);
+
+                plotPt.X = y + centreX;
+                plotPt.Y = -x + centreY;
+                putPixel(g, plotPt, (Brush) Brushes.Yellow);
+
+                plotPt.X = x + centreX;
+                plotPt.Y = -y + centreY;
+                putPixel(g, plotPt, (Brush) Brushes.Green);
+
+                plotPt.X = -x + centreX;
+                plotPt.Y = -y + centreY;
+                putPixel(g, plotPt, (Brush) Brushes.Blue);
+
+                plotPt.X = -y + centreX;
+                plotPt.Y = -x + centreY;
+                putPixel(g, plotPt, (Brush) Brushes.Indigo);
+
+                plotPt.X = -y + centreX;
+                plotPt.Y = x + centreY;
+                putPixel(g, plotPt, (Brush)Brushes.BlueViolet);
+
+                plotPt.X = -x + centreX;
+                plotPt.Y = y + centreY;
+                putPixel(g, plotPt, (Brush) Brushes.Violet);
+
+                // update d value 
+                if (d <= 0)
+                {
+                    d = d + 4 * x + 6;
+                }
+                else
+                {
+                    d = d + 4 * (x - y) + 10;
+                    y--;
+                }
+                x++;
+                Thread.Sleep(100);
+            }
+        }
+    } // end class
+}
---
+++ Week 4/ShapeRepresentation/ShapeRepresentation/VectorShape.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ShapeRepresentation
+{
+
+    public partial class VectorShape : Form
+    {
+        Point[] points = new Point[]
+        {
+            new Point(10, 10),
+            new Point(160, 10),
+            new Point(310, 10),
+            new Point(160, 85),
+            new Point(310, 85),
+            new Point(10, 160),
+            new Point(160, 160),
+            new Point(310, 160),
+            new Point(10, 235),
+            new Point(310, 235)
+        };
+
+        string[] lines = new string[]
+        {
+            "0,1",
+            "1,2",
+            "1,3",
+            "4,3",
+            "3,6",
+            "6,7",
+            "7,9",
+            "9,8",
+            "8,5",
+            "5,0",
+            "5,6",
+            "2,4"
+        };
+
+        public VectorShape()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen blackPen = new Pen(Color.Black);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] linePoints = lines[i].Split(',');
+                int p0index = Int32.Parse(linePoints[0]);
+                int p1index = Int32.Parse(linePoints[1]);
+
+                Point p0 = points[p0index];
+                Point p1 = points[p1index];
+
+                g.DrawLine(blackPen, p0, p1);
+            }
+        }
+    }
+}
